f5d32d17e7f45d83cb29824c13afc0d53759d24e
Abdullah Aswad <abdullahaswad2003@gmail.com>
2024-03-26 16:06:46 -0400

    Added rest of tables

--- DDL/Initialization.sql
+++ DDL/Initialization.sql
@@ -3,24 +3,6 @@ CREATE TABLE Profiles (
 	passwords TEXT NOT NULL
 );
 
--- Classes Table
-CREATE TABLE Classes (
-    class_id SERIAL PRIMARY KEY,
-	trainer_id TEXT,
-	FOREIGN KEY(trainer_id) REFERENCES Profiles(username),
-	class_date DATE,
-	class_type INT
-);
-
--- Class Relations
-CREATE TABLE Class_Members (
-    class_id INT,
-	FOREIGN KEY (class_id) REFERENCES Classes(class_id),
-	member_id TEXT,
-	FOREIGN KEY(member_id) REFERENCES Profiles(username),
-	class_date DATE
-);
-
 CREATE TABLE Members (
     username TEXT PRIMARY KEY,
 	first_name TEXT NOT NULL,
@@ -47,6 +29,11 @@ CREATE TABLE Members (
 	FOREIGN KEY (username) REFERENCES Profiles(username)
 );
 
+CREATE TABLE Rooms (
+	room_id INT PRIMARY KEY,
+	booking_status TEXT
+);
+
 CREATE TABLE Payment_Options (
 	payment_id SERIAL PRIMARY KEY,
 	member_id TEXT,
@@ -59,4 +46,52 @@ CREATE TABLE Exercise_Routines (
 	member_id TEXT,
 	exercise TEXT,
 	FOREIGN KEY (member_id) REFERENCES Profiles(username)
+);
+
+-- Classes Table
+CREATE TABLE Classes (
+    class_id SERIAL PRIMARY KEY,
+	trainer_id TEXT,
+	FOREIGN KEY(trainer_id) REFERENCES Profiles(username),
+	room_id INT,
+	FOREIGN KEY(room_id) REFERENCES Rooms(room_id),
+	class_type INT
+);
+
+CREATE TABLE Dates_Available (
+	trainer_id TEXT,
+	FOREIGN KEY(trainer_id) REFERENCES Profiles(username),
+	class_date DATE,
+	time_of_day TEXT
+);
+
+-- Class Relations
+CREATE TABLE Class_Members (
+    class_id INT,
+	FOREIGN KEY (class_id) REFERENCES Classes(class_id),
+	member_id TEXT,
+	FOREIGN KEY(member_id) REFERENCES Profiles(username),
+	class_date DATE
+);
+
+CREATE TABLE Bookings (
+	room_id INT,
+	FOREIGN KEY(room_id) REFERENCES Rooms(room_id),
+	class_date DATE,
+	time_of_day TEXT
+);
+
+CREATE TABLE Equipment (
+	equipment_id int PRIMARY KEY,
+	room_id INT,
+	FOREIGN KEY(room_id) REFERENCES Rooms(room_id),
+	equipment_status TEXT
+);
+
+CREATE TABLE Bills(
+	bill_id INT PRIMARY KEY,
+	member_id TEXT,
+	FOREIGN KEY(member_id) REFERENCES Profiles(username),
+	price INT,
+	date_issued date
 );
