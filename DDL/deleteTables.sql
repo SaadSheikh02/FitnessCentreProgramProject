7ea13ee3191c0538f3bd704bbbd4934327f14cc5
Saad Sheikh <saad1662002@gmail.com>
2024-03-27 15:19:35 -0400

    Added query to drop all tables

---
+++ DDL/deleteTables.sql
@@ -0,0 +1,10 @@
+DROP TABLE bills;
+DROP TABLE bookings;
+DROP TABLE class_members;
+DROP TABLE classes;
+DROP TABLE dates_trainer_unavailable;
+DROP TABLE equipment;
+DROP TABLE exercise_routines;
+DROP TABLE members;
+DROP TABLE profiles;
+DROP TABLE rooms;
