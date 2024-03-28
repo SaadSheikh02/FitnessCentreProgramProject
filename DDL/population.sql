64b5a905bb2475b0304e52bd3b9d8a0a140c808f
Abdullah Aswad <abdullahaswad2003@gmail.com>
2024-03-28 17:29:04 -0400

    created population file

---
+++ DDL/population.sql
@@ -0,0 +1,19 @@
+INSERT INTO 
+Profiles (username, passwords, first_name, last_name, user_type) 
+VALUES ('abdool', 'password123', 'Abdullah', 'Aswad', 'TYPE_ADMIN');
+
+INSERT INTO 
+Profiles (username, passwords, first_name, last_name, user_type) 
+VALUES ('squigz', 'compression', 'Saad', 'Sheikh', 'TYPE_ADMIN');
+
+INSERT INTO 
+Profiles (username, passwords, first_name, last_name, user_type) 
+VALUES ('sonic', 'speed', 'Sonic', 'Hedgehog', 'TYPE_TRAINER');
+
+INSERT INTO 
+Profiles (username, passwords, first_name, last_name, user_type) 
+VALUES ('shadow', 'edge', 'Shadow', 'Hedgehog', 'TYPE_TRAINER');
+
+INSERT INTO 
+Profiles (username, passwords, first_name, last_name, user_type) 
+VALUES ('knuckles', 'Punch', 'Knuckles', 'Echidna', 'TYPE_TRAINER');
