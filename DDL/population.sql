bc38154741164cc1b9688142aacc105c6face6a7
Saad Sheikh <saad1662002@gmail.com>
2024-04-05 17:08:38 -0400

    Modified population ddl file for testing

--- DDL/population.sql
+++ DDL/population.sql
@@ -24,4 +24,10 @@ VALUES ('jadfakhoury', 'football', 'Jad', 'Fakhoury', 'TYPE_MEMBER');
 
 INSERT INTO
 Members(username, credit_card, birthday, height, weight, diet_plan, goal_weight, goal_speed, goal_lift, weight_deadline, speed_deadline, lift_deadline, weight_loss, max_speed, max_lift, bmi, systolic_bp, diastolic_bp, heart_rate, bloodsugar_level)
-VALUES ('jadfakhoury', '123456789', '2003-01-29', 196, 87, 'keto', 80, 9, 225, '2024-06-16', '2024-06-17', '2024-06-18', 3, 8, 180, 25, 109, 68, 70, 83)
+VALUES ('jadfakhoury', '123456789', '2003-01-29', 196, 87, 'keto', 80, 9, 225, '2024-06-16', '2024-06-17', '2024-06-18', 3, 8, 180, 25, 109, 68, 70, 83);
+
+INSERT INTO Exercise_Routines(username, exercise)
+VALUES ('jadfakhoury', 'burpees'),
+('jadfakhoury', 'pushups'),
+('jadfakhoury', 'mountain climbers'),
+('jadfakhoury', 'situps');
